0ba2a34d81cee72152d5f77008e930d7a55c2826
SofiaBerezina <sofiaber114@gmail.com>
2023-12-12 23:47:40 +0300

    Добавлены изменения

--- check_format.sh
+++ check_format.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for file in *.txt; do
   if [[ $(file -b --mime-type "$file") = "text/plain" ]]; then
-    echo "Файл $file соответствует формату text/plain"
+    echo "Файл $file соответствует формату txt"
   else
-    echo "Файл $file не соответствует формату text/plain"
+    echo "Файл $file не соответствует формату txt"
   fi
 done
